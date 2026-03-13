1718e655d4efe671f89ecb38016a34c52c9ebdf9
Kineeeee <kienjssj@gmail.com>
2026-03-13 15:51:45 +0700

    fix CI REWARDS

--- apps/web/src/scenes/Preloader.js
+++ apps/web/src/scenes/Preloader.js
@@ -1,6 +1,6 @@
 import { Scene } from 'phaser';
 import { Logger } from '../utils/Logger';
-import { REWARDS, getGeneratedSkinTextureKey, getRewardPreloadEntries } from '../modules/achievements/RewardsCatalog';
+import { REWARDS, getGeneratedSkinTextureKey, getRewardPreloadEntries } from '../modules/achievements/RewardsCatalog.js';
 
 export class Preloader extends Scene {
     constructor() {
